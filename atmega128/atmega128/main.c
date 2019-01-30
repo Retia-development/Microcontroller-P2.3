70d301a0a7c57edeefcc5b81761840d42f702072
Tim <vries.timothy.de@gmail.com>
2019-01-30 10:43:57 +0100

    [created] Magics

---
+++ atmega128/atmega128/main.c
@@ -0,0 +1,18 @@
+/*
+ * atmega128.c
+ *
+ * Created: 30/01/2019 10:24:17
+ * Author : Tim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ atmega128/opdrachtenWeek1/main.c
@@ -0,0 +1,20 @@
+/*
+ * opdrachtenWeek1.c
+ *
+ * Created: 30/01/2019 10:26:22
+ * Author : Tim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+		//lets a gooo
+		
+    }
+}
+
+
