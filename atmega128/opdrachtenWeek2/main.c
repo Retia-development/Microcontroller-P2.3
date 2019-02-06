bb1f25a3b68b87ccd83088db4a9b87cac42c389c
Gerdtinus Netten <cornand1@live.nl>
2019-02-06 11:03:47 +0100

    [added] opdrachtenWeek2 poging 2

---
+++ atmega128/opdrachtenWeek2/main.c
@@ -0,0 +1,18 @@
+/*
+ * opdrachtenWeek2.c
+ *
+ * Created: 06-Feb-19 11:01:21 AM
+ * Author : corna
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
