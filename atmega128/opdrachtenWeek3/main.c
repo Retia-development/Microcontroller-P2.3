787db8d1f8ce3a8461a64f7c4ad753dfe3aa6e8f
svber <berningsven@gmail.com>
2019-02-13 10:46:04 +0100

    [added] week 3

---
+++ atmega128/opdrachtenWeek3/main.c
@@ -0,0 +1,18 @@
+/*
+ * opdrachtenWeek3.c
+ *
+ * Created: 2019-02-13 10:45:40
+ * Author : svber
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
